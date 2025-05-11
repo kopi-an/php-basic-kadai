86361a98f26ea062b424fb8e5485c960a9d34c95
kopi-an <clk.0251@gmail.com>
2025-05-11 18:56:15 +0900

    kadai_sortを追加

---
+++ kadai_sort/sort.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+    function sort_2way($nums,$order) {
+      [15, 4, 18, 23, 10 ];
+    
+    if ($order === true) {
+      echo '昇順にソートします。'.'<br>';
+      sort($nums);
+      foreach($nums as $num) {
+        echo $num.'<br>';
+      }
+    } else {
+      echo '降順にソートします。'.'<br>';
+      rsort($nums);
+      foreach($nums as $num) {
+        echo $num.'<br>';
+        }
+      }
+    }
+
+    $nums = [15, 4, 18, 23, 10];
+    sort_2way($nums, true);
+    sort_2way($nums, false);
+    ?>
+  </p>
+  
+</body>
+</html>
